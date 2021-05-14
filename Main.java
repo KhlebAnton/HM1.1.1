db522f2e1cfc24f53e7b409c70c9a3e25109d030
KhlebAnton <toni.farsh@icloud.com>
2021-05-14 14:48:38 +0500

    add files

---
+++ Main.java
@@ -0,0 +1,16 @@
+package Calculator;
+
+public class Main {
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1,1);
+        int c = calc.devide.apply(6,0);
+
+        calc.println.accept(c);
+
+
+    }
+}
